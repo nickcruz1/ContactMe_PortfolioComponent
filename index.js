af3080e013b0395aa8041bcd24b6f36e513a38f6
Nick Cruz <nickcruz920@gmail.com>
2023-02-08 11:07:16 -0500

    Added closeModal function

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ function contactMe() {
   let myModal = document.querySelector(".myModalO");
   myModal.style.display = "block";
 }
+
+function closeModal() {
+let myModal = document.querySelector(".myModalO");
+let closeModal = document.querySelector(".closeModal");
+  myModal.style.display = "none";
+  myModal.style.transition = "1s";
+
+}
